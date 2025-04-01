24955209f754b9a606148cd99d7207bdc09d600e
Andre Weber <andre.weber3@etas.com>
2025-04-01 08:22:30 +0200

    chore: Update Copyrights

--- databroker-examples/examples/slow_subscriber.rs
+++ databroker-examples/examples/slow_subscriber.rs
@@ -1,15 +1,17 @@
-/********************************************************************************
-* Copyright (c) 2024 Contributors to the Eclipse Foundation
-*
-* See the NOTICE file(s) distributed with this work for additional
-* information regarding copyright ownership.
-*
-* This program and the accompanying materials are made available under the
-* terms of the Apache License 2.0 which is available at
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* SPDX-License-Identifier: Apache-2.0
-********************************************************************************/
+/*
+ * *******************************************************************************
+ *  Copyright (c) 2024-2025 Contributors to the Eclipse Foundation
+ *
+ *  See the NOTICE file(s) distributed with this work for additional
+ *  information regarding copyright ownership.
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License 2.0 which is available at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ * ******************************************************************************
+ */
 
 use kuksa_rust_sdk::kuksa::common::to_uri;
 use kuksa_rust_sdk::kuksa::common::ClientTraitV1;
